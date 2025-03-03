9bdf0412c2f94e3f1264e55f9a4037e2243a9b0e
HFyilmaz <hyilmaz@sabanciuniv.edu>
2025-03-03 10:53:21 +0300

    favicon and page title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,19 @@
 import Theme from '../styles/theme';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
+  // Get the repository name from next.config.js
+  const repository = 'portfolio';
+  const isProduction = process.env.NODE_ENV === 'production';
+  const basePath = isProduction ? `/${repository}` : '';
+
   return (
     <>
+      <Head>
+        <title>Hasan Fırat Yılmaz | Portfolio</title>
+        <meta name="description" content="Hasan Fırat Yılmaz's Portfolio Website" />
+        <link rel="icon" href={`${basePath}/favicon.ico`} />
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -28,9 +28,17 @@ export default class MyDocument extends Document {
     }
   }
   render() {
+    // Get the repository name from next.config.js
+    const repository = 'portfolio';
+    const isProduction = process.env.NODE_ENV === 'production';
+    const basePath = isProduction ? `/${repository}` : '';
+    
     return (
       <Html lang='en-GB'>
         <Head>
+          <title>Hasan Fırat Yılmaz | Portfolio</title>
+          <meta name="description" content="Hasan Firat Yilmaz's Portfolio Website" />
+          <link rel="icon" href={`${basePath}/favicon.ico`} />
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
